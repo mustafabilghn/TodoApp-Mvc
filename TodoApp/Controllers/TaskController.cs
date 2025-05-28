565b35245b261f444312a5759b2fdba4ebf9288b
Mustafa Bilgehan Ateş <146623209+mustafabilghn@users.noreply.github.com>
2025-05-28 20:50:48 +0300

    Add project files.

---
+++ TodoApp/Controllers/TaskController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static List<TaskItem> _items = new();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_items);
+        }
+
+        [HttpPost]
+        public IActionResult Add(TaskItem item)
+        {
+            if (!_items.Contains(item))
+            {
+                _items.Add(new TaskItem
+                {
+                    Id = _items.Count + 1,
+                    Title = item.Title,
+                    IsCompleted = false,
+                    Date = DateTime.Now
+                });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Complete(int id)
+        {
+            var item = _items.FirstOrDefault(x => x.Id == id);
+
+            if (item != null)
+            {
+                item.IsCompleted = true;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int id)
+        {
+            var ItemToRemove = _items.FirstOrDefault(y => y.Id == id);
+
+            if (ItemToRemove != null)
+            {
+                _items.Remove(ItemToRemove);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var item = _items.FirstOrDefault(z => z.Id == id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TaskItem UpdatedItem)
+        {
+            var item = _items.FirstOrDefault(z => z.Id == UpdatedItem.Id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            item.Title = UpdatedItem.Title;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TodoApp/Models/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Başlık 50 karakterden fazla olamaz!")]
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
